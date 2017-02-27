8d0e2c3f0d55f8f53bb048cf4c17c70de7e2cea9
Volodymyr Medvid <vmed@cypress.com>
2017-02-27 11:42:43 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                 = 'tictactoe',
+    version              = '1.0.0',
+    description          = 'Tic-Tac-Toe game',
+    author               = 'Volodymyr Medvid',
+    author_email         = 'vmedvid@riseup.net',
+    url                  = 'http://github.com/medvid/tictactoe',
+    license              = 'MIT',
+    packages             = find_packages(),
+)
